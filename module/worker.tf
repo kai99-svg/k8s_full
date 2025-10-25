0055e4a4321d88fc5815946ee343e04543966a43
kai <yungkai0901@gmail.com>
2025-10-25 21:57:33 +0800

    added ansible to check nano

--- module/worker.tf
+++ module/worker.tf
@@ -162,7 +162,7 @@ resource "aws_instance" "foo" {
               sudo chmod +x kubectl
               sudo mv kubectl /usr/local/bin/
               sudo curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
-              sudo apt install -y ansible
+              sudo apt install -y ansible python3-boto3 python3-botocore
               EOF
 
   tags= {
